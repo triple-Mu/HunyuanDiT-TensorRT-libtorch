338e89befaa03a35683c9387e47411a23850d30e
triplemu <gpu@163.com>
2024-05-22 22:36:01 +0800

    fix findtensorrt.cmake

--- cmake/FindTensorRT.cmake
+++ cmake/FindTensorRT.cmake
@@ -41,6 +41,12 @@ if(NOT TensorRT_LIBRARY)
     endforeach()
 endif()
 
+if(NOT TensorRT_nvinfer_plugin_LIBRARY)
+    foreach(search ${_TensorRT_SEARCHES})
+        find_library(TensorRT_nvinfer_plugin_LIBRARY NAMES nvinfer_plugin ${${search}} PATH_SUFFIXES lib)
+    endforeach()
+endif()
+
 mark_as_advanced(TensorRT_INCLUDE_DIR)
 
 if(TensorRT_INCLUDE_DIR AND EXISTS "${TensorRT_INCLUDE_DIR}/NvInfer.h")
@@ -62,11 +68,63 @@ if(TensorRT_FOUND)
 
     if(NOT TensorRT_LIBRARIES)
         set(TensorRT_LIBRARIES ${TensorRT_LIBRARY})
+        if (TensorRT_nvinfer_plugin_LIBRARY)
+            list(APPEND TensorRT_LIBRARIES ${TensorRT_nvinfer_plugin_LIBRARY})
+        endif()
     endif()
 
     if(NOT TARGET TensorRT::TensorRT)
-        add_library(TensorRT::TensorRT UNKNOWN IMPORTED)
-        set_target_properties(TensorRT::TensorRT PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_INCLUDE_DIRS}")
-        set_property(TARGET TensorRT::TensorRT APPEND PROPERTY IMPORTED_LOCATION "${TensorRT_LIBRARY}")
+        add_library(TensorRT INTERFACE IMPORTED)
+        add_library(TensorRT::TensorRT ALIAS TensorRT)
+    endif()
+
+    if(NOT TARGET TensorRT::nvinfer)
+        add_library(TensorRT::nvinfer SHARED IMPORTED)
+        if (WIN32)
+            foreach(search ${_TensorRT_SEARCHES})
+                find_file(TensorRT_LIBRARY_DLL
+                        NAMES nvinfer.dll
+                        PATHS ${${search}}
+                        PATH_SUFFIXES bin
+                )
+            endforeach()
+
+            set_target_properties(TensorRT::nvinfer PROPERTIES
+                    INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_INCLUDE_DIRS}"
+                    IMPORTED_LOCATION "${TensorRT_LIBRARY_DLL}"
+                    IMPORTED_IMPLIB "${TensorRT_LIBRARY}"
+            )
+        else()
+            set_target_properties(TensorRT::nvinfer PROPERTIES
+                    INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_INCLUDE_DIRS}"
+                    IMPORTED_LOCATION "${TensorRT_LIBRARY}"
+            )
+        endif()
+        target_link_libraries(TensorRT INTERFACE TensorRT::nvinfer)
+    endif()
+
+    if(NOT TARGET TensorRT::nvinfer_plugin AND TensorRT_nvinfer_plugin_LIBRARY)
+        add_library(TensorRT::nvinfer_plugin SHARED IMPORTED)
+        if (WIN32)
+            foreach(search ${_TensorRT_SEARCHES})
+                find_file(TensorRT_nvinfer_plugin_LIBRARY_DLL
+                        NAMES nvinfer_plugin.dll
+                        PATHS ${${search}}
+                        PATH_SUFFIXES bin
+                )
+            endforeach()
+
+            set_target_properties(TensorRT::nvinfer_plugin PROPERTIES
+                    INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_INCLUDE_DIRS}"
+                    IMPORTED_LOCATION "${TensorRT_nvinfer_plugin_LIBRARY_DLL}"
+                    IMPORTED_IMPLIB "${TensorRT_nvinfer_plugin_LIBRARY}"
+            )
+        else()
+            set_target_properties(TensorRT::nvinfer_plugin PROPERTIES
+                    INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_INCLUDE_DIRS}"
+                    IMPORTED_LOCATION "${TensorRT_nvinfer_plugin_LIBRARY}"
+            )
+        endif()
+        target_link_libraries(TensorRT INTERFACE TensorRT::nvinfer_plugin)
     endif()
 endif()
